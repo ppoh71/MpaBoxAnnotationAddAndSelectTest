bb3eb15d6b0085d693d288251e20b2f28830a4c6
ppoh71 <ppoh71@gmail.com>
2020-09-10 16:45:44 +0100

    EnvironmentObject for UIViewRepresentable

--- MapboxSwiftUIDemo/ContentView.swift
+++ MapboxSwiftUIDemo/ContentView.swift
@@ -2,39 +2,47 @@ import SwiftUI
 import Mapbox
 
 struct ContentView: View {
+  @EnvironmentObject var annotationModel: AnnotationModel
+  
   @State var annotations: [MGLPointAnnotation] = [
-      MGLPointAnnotation(title: "Mapbox", coordinate: .init(latitude: 37.791434, longitude: -122.396267))
+    MGLPointAnnotation(title: "Mapbox", coordinate: .init(latitude: 37.791434, longitude: -122.396267))
   ]
   
-    var body: some View {
+  @State private var selectedAnnotaion: String = ""
+  
+  var body: some View {
+    VStack{
+      MapView(annotations: $annotations).centerCoordinate(.init(latitude: 37.791293, longitude: -122.396324)).zoomLevel(16).environmentObject(annotationModel)
+      
       VStack{
-        MapView(annotations: $annotations).centerCoordinate(.init(latitude: 37.791293, longitude: -122.396324)).zoomLevel(16)
-
         Button(action: {
-          /// working
-          self.addNextAnnotation(address: "Test")
+          self.addNextAnnotation(address: "Location: \(Int.random(in: 1..<1000))")
         }) {
-        Text("Add Annotation")
-        }.frame(height: 100)
+          Text("Add Location")
+        }.frame(width: 200, height: 50)
+        
+        Text("Selected: \(annotationModel.selectedAnnotaion)")
+        
+        Spacer().frame(height: 50)
       }
     }
+  }
   
   /// add a random annotion to the map
   /// - Parameter address: address description
   func addNextAnnotation(address: String) {
-    print("new anno", annotations)
     let randomLatitude = Double.random(in: 37.7912434..<37.7918434)
     let randomLongitude = Double.random(in: 122.396267..<122.396867) * -1
-    let newAnnotation = MGLPointAnnotation(title: "Anno \(address)", coordinate: .init(latitude: randomLatitude, longitude: randomLongitude))
-
+    let newAnnotation = MGLPointAnnotation(title: address, coordinate: .init(latitude: randomLatitude, longitude: randomLongitude))
+    
     annotations.append(newAnnotation)
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-      ContentView()
-    }
+  static var previews: some View {
+    ContentView().environmentObject(AnnotationModel())
+  }
 }
 
 
--- MapboxSwiftUIDemo/MapView.swift
+++ MapboxSwiftUIDemo/MapView.swift
@@ -1,118 +1,122 @@
 import SwiftUI
 import Mapbox
+import Combine
 
 extension MGLPointAnnotation {
-    convenience init(title: String, coordinate: CLLocationCoordinate2D) {
-        self.init()
-        self.title = title
-        self.coordinate = coordinate
-    }
+  convenience init(title: String, coordinate: CLLocationCoordinate2D) {
+    self.init()
+    self.title = title
+    self.coordinate = coordinate
+  }
 }
 
 struct MapView: UIViewRepresentable {
-    @Binding var annotations: [MGLPointAnnotation]
-    
-     let mapView: MGLMapView = MGLMapView(frame: .zero, styleURL: MGLStyle.streetsStyleURL)
-    
-    // MARK: - Configuring UIViewRepresentable protocol
-    
-    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MGLMapView {
-        mapView.delegate = context.coordinator
-        return mapView
+  @Binding var annotations: [MGLPointAnnotation]
+  @EnvironmentObject var annotationModel: AnnotationModel
+  
+  let mapView: MGLMapView = MGLMapView(frame: .zero, styleURL: MGLStyle.streetsStyleURL)
+  
+  // MARK: - Configuring UIViewRepresentable protocol
+  
+  func makeUIView(context: UIViewRepresentableContext<MapView>) -> MGLMapView {
+    mapView.delegate = context.coordinator
+    return mapView
+  }
+  
+  func updateUIView(_ uiView: MGLMapView, context: UIViewRepresentableContext<MapView>) {
+    updateAnnotations()
+  }
+  
+  func makeCoordinator() -> MapView.Coordinator {
+    Coordinator(self, annotationModel: _annotationModel)
+  }
+  
+  // MARK: - Configuring MGLMapView
+  
+  func styleURL(_ styleURL: URL) -> MapView {
+    mapView.styleURL = styleURL
+    return self
+  }
+  
+  func centerCoordinate(_ centerCoordinate: CLLocationCoordinate2D) -> MapView {
+    mapView.centerCoordinate = centerCoordinate
+    return self
+  }
+  
+  func zoomLevel(_ zoomLevel: Double) -> MapView {
+    mapView.zoomLevel = zoomLevel
+    return self
+  }
+  
+  private func updateAnnotations() {
+    if let currentAnnotations = mapView.annotations {
+      mapView.removeAnnotations(currentAnnotations)
     }
-    
-    func updateUIView(_ uiView: MGLMapView, context: UIViewRepresentableContext<MapView>) {
-      print("update view")
-        updateAnnotations()
-    }
-    
-    func makeCoordinator() -> MapView.Coordinator {
-        Coordinator(self)
-    }
-    
-    // MARK: - Configuring MGLMapView
-    
-    func styleURL(_ styleURL: URL) -> MapView {
-        mapView.styleURL = styleURL
-        return self
-    }
-    
-    func centerCoordinate(_ centerCoordinate: CLLocationCoordinate2D) -> MapView {
-        mapView.centerCoordinate = centerCoordinate
-        return self
-    }
-    
-    func zoomLevel(_ zoomLevel: Double) -> MapView {
-        mapView.zoomLevel = zoomLevel
-        return self
-    }
-    
-    private func updateAnnotations() {
-       print("updateAnnotations", annotations )
-      
-        if let currentAnnotations = mapView.annotations {
-          print("remove anno")
-           mapView.removeAnnotations(currentAnnotations)
-        }
-      mapView.addAnnotations(annotations)
-    }
-    
-    // MARK: - Implementing MGLMapViewDelegate
-    
-    final class Coordinator: NSObject, MGLMapViewDelegate {
-        var control: MapView
-        
-        init(_ control: MapView) {
-            self.control = control
-        }
-        
-        func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
-            
-            let coordinates = [
-                CLLocationCoordinate2D(latitude: 37.791329, longitude: -122.396906),
-                CLLocationCoordinate2D(latitude: 37.791591, longitude: -122.396566),
-                CLLocationCoordinate2D(latitude: 37.791147, longitude: -122.396009),
-                CLLocationCoordinate2D(latitude: 37.790883, longitude: -122.396349),
-                CLLocationCoordinate2D(latitude: 37.791329, longitude: -122.396906),
-            ]
-            
-            let buildingFeature = MGLPolygonFeature(coordinates: coordinates, count: 5)
-            let shapeSource = MGLShapeSource(identifier: "buildingSource", features: [buildingFeature], options: nil)
-            mapView.style?.addSource(shapeSource)
-            
-            let fillLayer = MGLFillStyleLayer(identifier: "buildingFillLayer", source: shapeSource)
-            fillLayer.fillColor = NSExpression(forConstantValue: UIColor.blue)
-            fillLayer.fillOpacity = NSExpression(forConstantValue: 0.5)
-            
-            mapView.style?.addLayer(fillLayer)
+    mapView.addAnnotations(annotations)
+  }
+  
+  // MARK: - Implementing MGLMapViewDelegate
+  
+  final class Coordinator: NSObject, MGLMapViewDelegate {
+    var control: MapView
+    @EnvironmentObject var annotationModel: AnnotationModel
 
-        }
-        
-        func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
-            return nil
-        }
-         
-        func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
-            return true
-        }
-      
-      func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
-        guard let annotationCollection = mapView.annotations else { return }
-        
-          /// check array for similar annotations
-        for _annotation in annotationCollection {
-          print("annotation", annotation)
-          
-          if annotation.coordinate.latitude == _annotation.coordinate.latitude {
-            /// this is the same annotation
-            print("*** Selected annoation")
-          } else {
-            print("--- Not the selected annoation")
-          }
-        }
-          mapView.setCenter(annotation.coordinate, zoomLevel: 17,  animated: true)
-      }
+    init(_ control: MapView, annotationModel: EnvironmentObject<AnnotationModel>) {
+      self.control = control
+      self._annotationModel = annotationModel
     }
+    
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+      
+      let coordinates = [
+        CLLocationCoordinate2D(latitude: 37.791329, longitude: -122.396906),
+        CLLocationCoordinate2D(latitude: 37.791591, longitude: -122.396566),
+        CLLocationCoordinate2D(latitude: 37.791147, longitude: -122.396009),
+        CLLocationCoordinate2D(latitude: 37.790883, longitude: -122.396349),
+        CLLocationCoordinate2D(latitude: 37.791329, longitude: -122.396906),
+      ]
+      
+      let buildingFeature = MGLPolygonFeature(coordinates: coordinates, count: 5)
+      let shapeSource = MGLShapeSource(identifier: "buildingSource", features: [buildingFeature], options: nil)
+      mapView.style?.addSource(shapeSource)
+      
+      let fillLayer = MGLFillStyleLayer(identifier: "buildingFillLayer", source: shapeSource)
+      fillLayer.fillColor = NSExpression(forConstantValue: UIColor.blue)
+      fillLayer.fillOpacity = NSExpression(forConstantValue: 0.5)
+      
+      mapView.style?.addLayer(fillLayer)
+      
+    }
+    
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+      return nil
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+      return true
+    }
+    
+    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
+      guard let annotationCollection = mapView.annotations else { return }
+      
+      /// cycle throu the annotations from the binding
+      /// @Binding var annotations: [MGLPointAnnotation]
+      for _annotation in annotationCollection {
+        print("annotation", annotation)
+        
+        if annotation.coordinate.latitude == _annotation.coordinate.latitude {
+          /// this is the same annotation
+          print("*** Selected annoation")
+          if let hastTitle = annotation.title {
+            annotationModel.selectedAnnotaion = hastTitle ?? "no string in title"
+          }
+        } else {
+          print("--- Not the selected annoation")
+        }
+      }
+      mapView.setCenter(annotation.coordinate, zoomLevel: 17,  animated: true)
+    }
+  }
 }
 
 
